8531218bc455513f40c5b5f2d5d8d92d237861f4
Manuel van Leeuwen <m.vanleeuwen12@student.han.nl>
2026-05-18 21:35:15 +0200

    Update About Me

--- resources/lang/en/about.php
+++ resources/lang/en/about.php
@@ -11,8 +11,13 @@ return [
         'I had almost no programming experience, and I watched some YouTube videos with explanation. ' .
         'Through building Discord bots, I learned JavaScript, Node.js, and eventually TypeScript. ' .
         'I also dived into the library (Discord.js), and it really helped me to start thinking at code-level. ' .
-        'Later, I also created some small Python projects, mainly for school.',
-    'p3'  => "Recently, I've started learning Laravel through its bootcamp project Chirper. " .
+        'Later, I also created some small Python projects, mainly for school.' .
+        "Recently, I've started learning Laravel through its bootcamp project Chirper. " .
         'I like it very much as a framework, it has everything you need, almost instantly ready to use. ' .
-        'It basically takes away all the painful stuff you needed to go through before when you would set up a website.'
+        'It basically takes away all the painful stuff you needed to go through before when you would set up a website. ',
+    'p3'  => "With Laravel, I built this portfolio. Isn't it beautiful? " .
+        'Well, that would be because the CSS is mostly written by Copilot. ' .
+        "I'm not a fan of designing websites, it takes much time and coding is just way more fun. " .
+        "Nevertheless, I'm still trying to learn it, because it will be useful. " .
+        'I also love to keep learning and exploring new technologies.'
 ];
--- resources/lang/nl/about.php
+++ resources/lang/nl/about.php
@@ -7,12 +7,17 @@ return [
     'p1'  => 'Ik ben Manuel van Leeuwen. Ik ben 21 jaar en ik woon in Nederland. ' .
         'In mijn vrije tijd speel ik graag tafeltennis, spreek ik af met vrienden en programmeer ik natuurlijk ook graag. ' .
         'Ik ben in mei dit jaar begonnen met een BOL Software Developer (niveau 4) bij Capabel Onderwijs, die ik in augustus 2027 hoop af te ronden.',
-    'p2'      => 'Ik begon met programmeren in 2020 door Discord bots te maken. ' .
+    'p2'  => 'Ik begon met programmeren in 2020 door Discord bots te maken. ' .
         "Ik had bijna geen programmeerervaring en heb wat YouTube-video's met uitleg gekeken. " .
         'Door het bouwen van Discord bots leerde ik JavaScript, Node.js en uiteindelijk TypeScript. ' .
         'Ik dook ook in de library (Discord.js), wat me echt hielp om op code-level na te denken. ' .
-        'Later heb ik ook wat kleine projecten in Python gemaakt, voornamelijk voor school.',
-    'p3'      => "Sinds kort ben ik Laravel gaan leren via het bootcamp project Chirper. " .
+        'Later heb ik ook wat kleine projecten in Python gemaakt, voornamelijk voor school.' .
+        "Sinds kort ben ik Laravel gaan leren via het bootcamp project Chirper. " .
         'Ik hou enorm van Laravel als framework, het heeft alles wat je nodig hebt, en je kan alles bijna direct gebruiken zonder gedoe. ' .
-        'Het neemt eigenlijk alles weg wat niet leuk is en veel tijd kost wat je normaal wel zou moeten doen als je een website gaat bouwen.'
+        'Het neemt eigenlijk alles weg wat niet leuk is en veel tijd kost wat je normaal wel zou moeten doen als je een website gaat bouwen.',
+    'p3'  => 'Met Laravel heb ik dit portfolio opgebouwd. Is het niet prachtig? ' .
+        'Nou, dat komt omdat het meeste van de CSS door Copilot is geschreven' .
+        'Ik hou niet zo van het designen van websites, het kost veel tijd en programmeren is gewoon veel leuker. ' .
+        'Toch probeer ik het nog steeds te leren, omdat het wel heel nuttig gaat zijn. ' .
+        'Ik hou er ook van om nieuwe technologieën te blijven leren en ontdekken.'
 ];
